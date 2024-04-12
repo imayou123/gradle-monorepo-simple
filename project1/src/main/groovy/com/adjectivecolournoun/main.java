89b6dfb4885f033ca52036b6dbc94ce4fe7a7f45
imayou123 <144216213+imayou123@users.noreply.github.com>
2024-04-12 11:50:06 +0200

    Update main.java

--- project1/src/main/groovy/com/adjectivecolournoun/main.java
+++ project1/src/main/groovy/com/adjectivecolournoun/main.java
@@ -3,6 +3,6 @@ package  com.adjectivecolournoun
 public class Main {
 
     public static void main(String... args) {
-        System.out.println('test1');
+        System.out.println("test1");
     }
 }
